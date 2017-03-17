dbcd46a256f870073bbf12d18ec851e5f18cf504
alexcost2002 <alexcost2002@gmail.com>
2017-03-17 20:49:25 -0300

    Add files via upload
    Exercício de Vetores

---
+++ Exercicio_1.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main (){
+
+  int i;
+  int vetor[10] = {1,2,3,4,5,6,7,8,9,10};
+
+  for( i = 0; i < 10; i++) {
+    printf("Vetor = %d\n", vetor[i]);
+  }
+    
+  return (0);
+
+}
